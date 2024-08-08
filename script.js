616b383860b7c276fc6c7f77224ba16ebe791225
kennimartinez27 <kenniandresm@gmail.com>
2024-08-07 21:48:23 -0500

    menu responsive

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript para activar/desactivar el menú
+document.getElementById('icono-nav').addEventListener('click', function() {
+    document.getElementById('nav').classList.toggle('active');
+});
